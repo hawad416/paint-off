27533109bb6b9b5015d0b8cdd3438643d559831e
Isaac Wu <isaacwu@uw.edu>
2026-06-06 20:20:01 -0700

    fixed constants

--- constants.js
+++ constants.js
@@ -38,7 +38,7 @@ const CONSTANTS = {
   AI_MODEL: 'claude-sonnet-4-20250514',
 
   // Bluetooth
-  NUS_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
-  NUS_TX_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e",
-  NUS_RX_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e",
+  NUS_SERVICE_UUID: "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
+  NUS_TX_UUID: "6e400003-b5a3-f393-e0a9-e50e24dcca9e",
+  NUS_RX_UUID: "6e400002-b5a3-f393-e0a9-e50e24dcca9e",
 }
